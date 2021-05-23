e37c6fa9477ee3792bdb15da540bc4403f0ee7fc
migellamp <80758137+migellamp@users.noreply.github.com>
2021-05-23 18:50:31 +0700

    Revisi soal2b.c

--- soal2/soal2b.c
+++ soal2/soal2b.c
@@ -1,37 +1,67 @@
-// MASIH SALAH (DIREVISI DULU NANTI)
-// Kendala : membandingkan 2 matrix
-
+// SESUDAH REVISI
 #include <stdio.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
 
-// Fungsi Faktorial
-int faktorial(int num) {
-  if(num == 0){
-    return 1;
-  }
-  return num * faktorial(num-1);
+int matriks_kedua[24];
+int *value;
+
+
+// Membuat struct yang mana menyimpan 2 variable, yaitu matriks pertama dan matriks kedua
+typedef struct matrix {
+    int matriks_pertama;
+    int matriks_kedua;
+}matrix;
+
+// Fungsi yang digunakan untuk ketentuan dalam faktorial
+long long ketentuan(long long b, int testcase)
+{
+	long long ans=1;
+	for (long long a=0;a<testcase;a++)
+	{
+        // ketika b > a
+		if(b-a <= 0)
+			ans *= 1;
+        // ketika a > b
+		else
+			ans *= (b-a);
+	}
+    // Retutn nilai ans
+	return ans;
 }
 
-int main() {
-    
-    int lebihdari = 1;
-    int kurangdari = 1;
-    int test=5;
-    int test1=10;
-    int test2=3;
-    int matriks1[100][100], matriks2[100][100], hasil[100][100];
-    int i, j, k, m, n, p, q, jumlah = 0;
+// Fungsi Faktorial
+void *faktorial (void *input)
+{
+	matrix *temp = (matrix *)input;
+	if((*temp).matriks_kedua == 0 || (*temp).matriks_pertama == 0)
+    {
+        printf("0\t");  
+    }	
+	else
+    {
+        printf("%lld\t", ketentuan((*temp).matriks_pertama, (*temp).matriks_kedua));
+    }
+		
+}
 
-    key_t key = 1234;
-    int *value;
+void main()
+{
+    // Shared Memory
+	pthread_t thread;
+	key_t key = 1234;
 
+    //int *temp_matriks_pertama;
     int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
+
     value = shmat(shmid, NULL, 0);
 
     sleep(5);
 
+    // Menampilkan Matriks Pertama
     printf ("Matriks Pertama: \n");
     for(int i=0;i<4;i++)
     {
@@ -41,52 +71,33 @@ int main() {
     	}
         printf ("\n");
     }
-    printf ("\n");
-
-    p=4;
-    q=6;
-
-    printf("Masukkan Matriks Kedua: \n");
-    for(i = 0; i < p; i++)
+    printf ("\n");	
+	
+    // Memasukkan input ke matriks kedua
+	printf("Masukkan Matriks Kedua: \n");
+    for(int i=0;i<24;i++)
     {
-      for(j = 0; j < q; j++)
-      {
-        scanf("%d", &matriks2[i][j]);
-      }
+        //printf("Masukkan data input[%d]:", i+1);
+        scanf("%d", &matriks_kedua[i]);
     }
-    for(i = 0; i < p; i++)
-    {
-      for(j = 0; j < q; j++)
-      {
-          // a lebih kecil dari b
-          if(value[i*6+j] >> matriks2[i][j])
-          {
-                value[i*6+j] = faktorial(value[i*6+j]);
-                matriks2[i][j] = 1;
-          }
-          else if (value[i*6+j] == 0 || matriks2[i][j] == 0)
-          {
-                value[i*6+j] = 0;
-                matriks2[i][j] = 1;
-          }
-          // a lebih besar dari b
-          else if(value[i*6+j] << matriks2[i][j])
-          {
-                value[i*6+j] = faktorial(value[i*6+j])/faktorial(value[i*6+j]-matriks2[i][j]);
-                matriks2[i][j] = 1;
-          }
+    printf ("\n");	
+	
+    // Mengkalikan matriks menggunakan thread
+	printf("Matriks yang dihasilkan: \n");
+    for(int j=0;j<24;j++)
+	{
+        if(j%6 == 0)
+            printf("\n");
+            
+        matrix *temp1 = (matrix  *)malloc(sizeof(*temp1)); 
 
-        hasil[i][j] = value[i*6+j] * matriks2[i][j];
-      }
+        (*temp1).matriks_pertama= value[j];
+        (*temp1).matriks_kedua = matriks_kedua[j];
+        
+        pthread_create(&thread, NULL, faktorial, (void *)temp1); 
+        pthread_join(thread,NULL); 
     }
-
-    printf("Hasil perkalian matriks: \n");
-    for(i = 0; i < p; i++){
-      for(j = 0; j < q; j++){
-        printf("%d\t", hasil[i][j]);
-      }
-      printf("\n");
-    }
+    printf("\n");
 
     shmdt(value);
     shmctl(shmid, IPC_RMID, NULL);
